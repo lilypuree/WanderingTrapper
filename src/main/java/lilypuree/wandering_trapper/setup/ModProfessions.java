1b4088cd1d29ad8b6980a7f8c57996f6568c74d9
lilypuree <000abczyx@naver.com>
2020-06-19 02:10:36 +0900

    Fixed fox/polar bear issue, added russian translation, fixed obfucastion issues

--- src/main/java/lilypuree/wandering_trapper/setup/ModProfessions.java
+++ src/main/java/lilypuree/wandering_trapper/setup/ModProfessions.java
@@ -5,6 +5,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.entity.merchant.villager.VillagerProfession;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.village.PointOfInterestType;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
 import javax.annotation.Nullable;
 import java.lang.reflect.Constructor;
@@ -18,39 +19,37 @@ public class ModProfessions {
     private static Constructor professionConstructor;
     private static Method blockStatesInjector;
 
-    static{
-        try{
-//            poiConstructor = PointOfInterestType.class.getDeclaredConstructor(String.class, Set.class, int.class, int.class);
-//            poiConstructor.setAccessible(true);
-//            professionConstructor = VillagerProfession.class.getDeclaredConstructor(String.class, PointOfInterestType.class, ImmutableSet.class, ImmutableSet.class, SoundEvent.class);
-//            professionConstructor.setAccessible(true);
-            blockStatesInjector = PointOfInterestType.class.getDeclaredMethod("func_221052_a", PointOfInterestType.class);
+    static {
+        try {
+            poiConstructor = ObfuscationReflectionHelper.findConstructor(PointOfInterestType.class, String.class, Set.class, int.class, int.class);
+            poiConstructor.setAccessible(true);
+            professionConstructor = ObfuscationReflectionHelper.findConstructor(VillagerProfession.class, String.class, PointOfInterestType.class, ImmutableSet.class, ImmutableSet.class, SoundEvent.class);
+            professionConstructor.setAccessible(true);
+            blockStatesInjector = ObfuscationReflectionHelper.findMethod(PointOfInterestType.class, "func_221052_a", PointOfInterestType.class);
             blockStatesInjector.setAccessible(true);
-        }catch(NoSuchMethodException | SecurityException e) {
+        } catch (SecurityException e) {
             e.printStackTrace();
         }
     }
 
     public static PointOfInterestType pointOfInterestType(String resourceLocation, Set<BlockState> blockStates, int maxFreeTickets, int p_i225712_4_) {
         try {
-//            PointOfInterestType poi = (PointOfInterestType) poiConstructor.newInstance(resourceLocation, blockStates, maxFreeTickets, p_i225712_4_);
-            PointOfInterestType poi = new PointOfInterestType(resourceLocation, blockStates,maxFreeTickets, null, p_i225712_4_);
+            PointOfInterestType poi = (PointOfInterestType) poiConstructor.newInstance(resourceLocation, blockStates, maxFreeTickets, p_i225712_4_);
             blockStatesInjector.invoke(null, poi);
             return poi;
-        }catch (IllegalAccessException | InvocationTargetException e){
+        } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    public static VillagerProfession villagerProfession(String resourceLocation, PointOfInterestType type, @Nullable SoundEvent soundEvent){
-//        try {
-//            VillagerProfession poi = (VillagerProfession) professionConstructor.newInstance(resourceLocation, type, ImmutableSet.of(), ImmutableSet.of(), soundEvent);
-            VillagerProfession profession = new VillagerProfession(resourceLocation, type, ImmutableSet.of(), ImmutableSet.of());
-            return profession;
-//        }catch (){
-////            e.printStackTrace();
-//        }
-//        return null;
+    public static VillagerProfession villagerProfession(String resourceLocation, PointOfInterestType type, @Nullable SoundEvent soundEvent) {
+
+        try {
+            return (VillagerProfession) professionConstructor.newInstance(resourceLocation, type, ImmutableSet.of(), ImmutableSet.of(), soundEvent);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
